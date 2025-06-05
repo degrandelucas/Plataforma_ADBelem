9ce73d5c56fa18d19fca69239c667e30f045155c
degrandelucas <lucasdegrande15@gmail.com>
2025-06-05 17:36:51 -0300

    alterando para carregar css primeiro que arquivos js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
-        @vite(['resources/js/app.js', 'resources/js/utilities.js', 'resources/css/app.css'])
+        @vite(['resources/css/app.css','resources/js/app.js', 'resources/js/utilities.js'])
     </head>
 
     <body class="grey lighten-5">
